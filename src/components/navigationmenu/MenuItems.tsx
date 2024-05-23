26d3068e84805eeb3c4f1efd423c3d2ed240ab7c
birisi atuya <tushhamphrey@gamil.com>
2024-05-23 23:27:56 +0300

    build navigation menu

---
+++ src/components/navigationmenu/MenuItems.tsx
@@ -0,0 +1,23 @@
+export default function MenuItems() {
+  return (
+    <div>
+      <div className="py-5 d-flex gap-5 justify-content-center border-bottom">
+        <a href="#" className="link-dark text-decoration-none border-bottom">
+          <h5>HOME</h5>
+        </a>
+        <a href="#" className="link-dark text-decoration-none">
+          <h5>ABOUT</h5>
+        </a>
+        <a href="#" className="link-dark text-decoration-none">
+          <h5>LATEST</h5>
+        </a>
+        <a href="#" className="link-dark text-decoration-none">
+          <h5>STARTUPS</h5>
+        </a>
+        <a href="#" className="link-dark text-decoration-none">
+          <h5>POLICY & GOVERNANCE</h5>
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/HomePageLayout.tsx
+++ src/pages/home/HomePageLayout.tsx
@@ -1,9 +1,11 @@
 import LogosectionLayout from "../../components/logosection/LogosetionLayout";
+import MenuItems from "../../components/navigationmenu/MenuItems";
 
 export default function HomepageLayout() {
   return (
     <>
       <LogosectionLayout />
+      <MenuItems />
     </>
   );
 }
