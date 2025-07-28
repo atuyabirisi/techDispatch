19ce1fdebd0b124869df5a02a7a50599c4f9eaf9
atuyallday <birisiha@ueab.ac.ke>
2025-07-28 14:36:10 +0300

    sample changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import BlogPostPage from "./pages/BlogPostPage";
+import SamplePage from "./pages/Sample";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/posts/:id" element={<BlogPostPage />} />
+        <Route path="/sample" element={<SamplePage />} />
       </Routes>
     </BrowserRouter>
   );
